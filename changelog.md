11fb1f4b129c4d2f832ceb922c7e3092b98b0a3a
fabiorzfreitas <mckfabio@gmail.com>
2023-02-19 20:40:59 -0300

    v0.3.0 - Almost fully functional

--- changelog.md
+++ changelog.md
@@ -1,2 +1,8 @@
-0.1.0 - Basic functionality seems ok
-0.0.1 - Not working yet
+**<span style="color:#56adda">0.3.0</span>**
+- Almost fully functional, it still needs to parse chapters, attachments and metadata. The rest works.
+
+**<span style="color:#56adda">0.2.0</span>**
+- Basic functionality seems ok.
+
+**<span style="color:#56adda">0.1.0</span>**
+- Not working yet.
--- plugin.py
+++ plugin.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    fabiorzfreitas_preset.plugin.py
+ 
+    Written by:               Fabiorzfreitas <mckfabio@gmail.com>
+    Date:                     Sunday, February 2nd, 2023, 13:00
+ 
+    Copyright:
+           Copyright (C) Josh Sunnex - All Rights Reserved
+ 
+           Permission is hereby granted, free of charge, to any person obtaining a copy
+           of this software and associated documentation files (the "Software"), to deal
+           in the Software without restriction, including without limitation the rights
+           to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+           copies of the Software, and to permit persons to whom the Software is
+           furnished to do so, subject to the following conditions:
+  
+           The above copyright notice and this permission notice shall be included in all
+           copies or substantial portions of the Software.
+  
+           THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+           EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+           MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+           IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+           DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+           OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+           OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
 import logging
 
 import os
@@ -24,6 +53,12 @@ def on_library_management_file_test(data):
     :param data:
     :return:
     """
+  
+    abspath = data.get('path')
+
+    logger.debug('------------')
+    logger.debug(f'Testing file {abspath}')
+    logger.debug('------------')
 
     # Get file probe
     probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
@@ -34,9 +69,24 @@ def on_library_management_file_test(data):
     ffprobe_data = probe.get_probe()
 
     # Get the path to the file
-    abspath = data.get('path')
 
     shared_info = {}
+
+    if os.path.dirname(abspath) == 'Optimized for TV':
+
+        logger.debug(f'File already has been optimized')
+
+        data['add_file_to_pending_tasks'] = False
+
+        return
+
+    for stream in ffprobe_data['streams']:
+        
+        if stream['codec_type'] == 'video' and stream['codec_name'] != 'h264':
+
+            logger.debug(f'Video stream is not x264')
+
+            shared_info['non_h264'] = True
 
     if ffprobe_data['streams'][0]['codec_type'] != 'video':
         
@@ -47,7 +97,8 @@ def on_library_management_file_test(data):
             if stream['codec_type'] == 'video':
                 shared_info['non_0_video_stream'] = True
                 shared_info['video_stream_index'] = stream['index']
-        # This doesn't return yet, as a match with other cases is still possible
+                
+                return
 
     if ffprobe_data['streams'][1]['codec_type'] == 'audio' and ffprobe_data['streams'][1]['codec_name'] != 'ac3':
         
@@ -80,8 +131,6 @@ def on_library_management_file_test(data):
     return
             
 
-
-
 def on_worker_process(data):
     """
     Runner function - enables additional configured processing jobs during the worker stages of a task.
@@ -101,11 +150,18 @@ def on_worker_process(data):
     
     """
 
+    # Get the path to the file
+    abspath = data['original_file_path']
+
+    logger.debug('------------')
+    logger.debug(f'Processing file {abspath}')
+    logger.debug('------------')
+
     file_in = data['file_in']
     data['path'] = data['original_file_path']
-    data['file_out'] = 'C:\\Users\\Fabio\\Desktop\\samples\\cache.mkv'
+    data['file_out'] = 'C:\\Users\\Fabio\\Desktop\\samples\\cache\\cache.mkv'
     file_out = data['file_out']
-
+    video_codec = '-c:v:0 copy'
 
     # Get file probe
     probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
@@ -115,13 +171,20 @@ def on_worker_process(data):
 
     ffprobe_data = probe.get_probe()
 
-    # Get the path to the file
-    abspath = data.get('original_file_path')
 
     # Set the parser
     parser = Parser(logger)
     parser.set_probe(probe)
     data['command_progress_parser'] = parser.parse_progress
+
+    for stream in ffprobe_data['streams']:
+        
+        if stream['codec_type'] == 'video' and stream['codec_name'] != 'h264':
+
+            logger.debug(f'Video stream is not x264')
+
+            video_codec = '-c:v:0 h264'
+            break
 
     if ffprobe_data['streams'][0]['codec_type'] != 'video':
         
@@ -129,30 +192,37 @@ def on_worker_process(data):
         
         for stream in data['streams']:
             if stream['codec_type'] == 'video':
-                data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+                data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
 
         # This doesn't return yet, as a match with other cases is still possible
 
     if ffprobe_data['streams'][1]['codec_type'] == 'audio' and ffprobe_data['streams'][1]['codec_name'] != 'ac3':
         
         logger.debug(f'File {abspath} does not have ac3 as the first audio stream, processing')
-        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a:0 -c:a:0 ac3 -map 0:a:0 -c:a:1 copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a:0 -c:a:0 ac3 -map 0:a:0 -c:a:1 copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
         
         return
 
+    if video_codec == '-c:v:0 x264':
+        
+        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+
+        return
+
+    
     for stream in ffprobe_data['streams']:
        
        if stream['codec_type'] == 'subtitle':
             
             logger.debug(f'File {abspath} has subtitles, processing')
-            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
             
             return
        
        if stream['codec_type'] != 'audio':
 
             logger.debug(f'File {abspath} has non-audio, non-subtitle stream, likely an attachment, processing')
-            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 {video_codec} -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_out}'
 
             return
 
@@ -174,13 +244,44 @@ def on_postprocessor_file_movement(data):
     :return:
     """
 
+    data['path'] = data['source_data']['abspath']
+
+    abspath = data['path']
+
+    logger.debug('------------')
+    logger.debug(f'Post-processing file {abspath}')
+    logger.debug('------------')
+
+    # Get file probe
+    probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
+    if not probe:
+        # File not able to be probed by ffprobe
+        return
+
+    ffprobe_data = probe.get_probe()
+   
+    source_dirpath = f"{os.path.split(data['source_data']['abspath'])[0]}"
+    source_dirpath_replaced = source_dirpath.replace('\\', '\\\\')
+    show_dir = source_dirpath_replaced.split('\\')[-2]
+
     data['remove_source_file'] = False
     data['copy_file'] = True
     data['run_default_file_copy'] = False
+    data['file_out'] = f'{source_dirpath_replaced}\\test.mkv'
+    basename = data['source_data']['basename']
 
-    source_dir = f"{os.path.split(data['source_data']['abspath'])[0]}"
-    source_dir_replaced = source_dir.replace('\\', '\\\\')
+    for stream in ffprobe_data['streams']:
+        
+        if stream['codec_type'] == 'video' and stream['codec_name'] != 'h264':
 
-    data['file_out'] = f'{source_dir_replaced}\\test.mkv'
+            logger.debug(f'Video stream is not x264')
+
+            os.makedirs(f'{source_dirpath_replaced}\\\Plex Versions\\\\Optimized for TV\\\\{show_dir}', exist_ok= True)
+
+            data['file_out'] = f'{source_dirpath_replaced}\\\\Plex Versions\\\\Optimized for TV\\\\{show_dir}{basename}'
+            
+            break
+
 
     return
+
