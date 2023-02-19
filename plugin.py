0680f1fcdcf6d6603fc632b36a79ee157d2e38c0
fabiorzfreitas <mckfabio@gmail.com>
2023-02-19 12:03:12 -0300

    First draft, should be working.

--- plugin.py
+++ plugin.py
@@ -34,15 +34,50 @@ def on_library_management_file_test(data):
     # Get the path to the file
     abspath = data.get('path')
 
-    if ffprobe_data[0]['codec_type'] != 'video':
-        logger.debug(f'File {abspath} does not have video as the first stream')
+    shared_info = {}
+
+    if ffprobe_data['streams'][0]['codec_type'] != 'video':
+        
+        logger.debug(f'File {abspath} does not have video as the first stream, adding to queue')
         data['add_file_to_pending_tasks'] = True
+        
+        for stream in data['streams']:
+            if stream['codec_type'] == 'video':
+                shared_info['non_0_video_stream'] = True
+                shared_info['video_stream_index'] = stream['index']
+        # This doesn't return yet, as a match with other cases is still possible
+
+    if ffprobe_data['streams'][1]['codec_type'] == 'audio' and ffprobe_data[1]['codec_name'] != 'ac3':
+        
+        logger.debug(f'File {abspath} does not have ac3 as the first audio stream, adding to queue')
+        data['add_file_to_pending_tasks'] = True
+        shared_info['first_audio_is_not_ac3'] = True
+        
         return
 
-    if ffprobe_data[1]['codec_type'] == 'audio' and ffprobe_data[1]['codec_name'] != 'ac3':
-        logger.debug(f'File {abspath} does not have ac3 as the first audio stream')
-        data['add_file_to_pending_tasks'] = True
-        return
+    for stream in ffprobe_data['streams']:
+       
+       if stream['codec_type'] == 'subtitle':
+            
+            logger.debug(f'File {abspath} has subtitles, adding to queue')
+            data['add_file_to_pending_tasks'] = True
+            shared_info['has_subtitles'] = True
+            
+            return
+       
+       if stream['codec_type'] != 'audio':
+
+            logger.debug(f'File {abspath} has non-audio, non-subtitle stream, likely an attachment, adding to queue')
+            data['add_file_to_pending_tasks'] = True
+            shared_info['has_attachment'] = True
+
+            return
+
+    logger.debug(f"File {abspath} doesn't need processing, skipping")
+    data['add_file_to_pending_tasks'] = False
+    return
+            
+
 
 
 def on_worker_process(data):
@@ -63,4 +98,54 @@ def on_worker_process(data):
     :return:
     
     """
+
+    abspath = data['original_file_path']
+    file_in = data['file_in']
+
+    # Get file probe
+    probe = Probe.init_probe(data, logger, allowed_mimetypes=['video'])
+    if not probe:
+        # File not able to be probed by ffprobe
+        return
+
+    ffprobe_data = probe.get_probe()
+
+    # Set the parser
+    parser = Parser(logger)
+    parser.set_probe(probe)
+    data['command_progress_parser'] = parser.parse_progress
+
+    if ffprobe_data['streams'][0]['codec_type'] != 'video':
+        
+        logger.debug(f'File {abspath} does not have video as the first stream, processing')
+        
+        for stream in data['streams']:
+            if stream['codec_type'] == 'video':
+                data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_in}'
+
+        # This doesn't return yet, as a match with other cases is still possible
+
+    if ffprobe_data['streams'][1]['codec_type'] == 'audio' and ffprobe_data[1]['codec_name'] != 'ac3':
+        
+        logger.debug(f'File {abspath} does not have ac3 as the first audio stream, processing')
+        data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a:0 -c:a:0 ac3 -map 0:a:0 -c:a:1 copy -sn -map_metadata -1 -map_chapters -1 {file_in}'
+        
+        return
+
+    for stream in ffprobe_data['streams']:
+       
+       if stream['codec_type'] == 'subtitle':
+            
+            logger.debug(f'File {abspath} has subtitles, processing')
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_in}'
+            
+            return
+       
+       if stream['codec_type'] != 'audio':
+
+            logger.debug(f'File {abspath} has non-audio, non-subtitle stream, likely an attachment, processing')
+            data['exec_command'] = f'ffmpeg -i {file_in} -map 0:v:0 -c:v:0 copy -map 0:a -c:a copy -sn -map_metadata -1 -map_chapters -1 {file_in}'
+
+            return
+
     return
